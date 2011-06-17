5590a6c64d7a665d8807a8234cb2a5ce68297c2a
beawesomeinstead <graf.otodrakula@gmail.com>
2011-06-17 14:01:43 +0300

    Add basic auth

--- ruby_shift.rb
+++ ruby_shift.rb
@@ -2,6 +2,10 @@ class RubyShift < Sinatra::Base
   set :root,   File.dirname(__FILE__)
   set :public, File.join(root, 'public')
 
+  use Rack::Auth::Basic, "Restricted Area" do |username, password|
+    username == 'foo'
+  end
+
   get '/' do
     erb(:index)
   end
