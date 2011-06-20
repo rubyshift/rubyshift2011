37ba8d9be7f30d9f5eb9406f88ff11bd3ad5a572
beawesomeinstead <graf.otodrakula@gmail.com>
2011-06-20 15:44:31 +0300

    Remove basic auth

--- ruby_shift.rb
+++ ruby_shift.rb
@@ -2,9 +2,9 @@ class RubyShift < Sinatra::Base
   set :root,   File.dirname(__FILE__)
   set :public, File.join(root, 'public')
 
-  use Rack::Auth::Basic, "Restricted Area" do |username, password|
-    username == 'foo'
-  end
+  # use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  #   username == 'foo'
+  # end
 
   get '/' do
     erb(:index)
