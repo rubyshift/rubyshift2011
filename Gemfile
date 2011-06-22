7c6be5f9d3943e505527768120f371208f01f843
beawesomeinstead <graf.otodrakula@gmail.com>
2011-06-22 14:21:13 +0300

    Add SASS and Compass.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'http://rubygems.org'
 
+gem 'compass'
 gem 'sinatra'
+gem 'sass'
 
 group :test do
   gem 'rack-test', :require => 'rack/test'
--- ruby_shift.rb
+++ ruby_shift.rb
@@ -1,12 +1,17 @@
 class RubyShift < Sinatra::Base
+  configure do
+    Compass.add_project_configuration(File.join(Sinatra::Application.root, 'compass.config'))
+  end
+
   set :root,   File.dirname(__FILE__)
   set :public, File.join(root, 'public')
 
-  # use Rack::Auth::Basic, "Restricted Area" do |username, password|
-  #   username == 'foo'
-  # end
-
   get '/' do
     erb(:index)
+  end
+
+  get '/screen.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    scss :screen, Compass.sass_engine_options
   end
 end
